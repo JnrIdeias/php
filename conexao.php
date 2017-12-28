3613458232d46ae4bd92990e08b94da442bd8c24
Junior <reissilva@hotmail.com>
2017-12-28 19:51:39 -0200

    Novos testes.

---
+++ conexao.php
@@ -0,0 +1,24 @@
+<?php
+$host="localhost";
+$port=3306;
+$socket="";
+$user="root";
+$password="VaiNao";
+$dbname="udemybd";
+
+/* Gerado pelo MySql-Workbench
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
+
+*/
+    // 1. Abrir conexao
+    $conecta = mysqli_connect($host, $user, $password, $dbname);
+
+    // 2. Testar conexao
+    if (mysqli_connect_errno()) {
+        die("Conexão falou : " . mysqli_connect_errno());
+    }
+?>
